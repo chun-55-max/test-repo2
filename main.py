78d77641be4d12e06aca5598b749e404737c5bd8
hun <ochanghun01@gmail.com>
2025-12-09 07:56:45 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#main.py
+
+#input
+first = input("Enter the first number")
+second = input("Enter the second number")
+
+num1 = int(first)
+num2 = int(second)
+
+result = num1 + num2
+
+printf("sum:", result)
+
